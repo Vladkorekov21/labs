1daddd27bab2cd7295b43ccde04ab6f359a1c64a
Vladkorekov21 <74411502+Vladkorekov21@users.noreply.github.com>
2021-03-01 14:39:34 +0500

    8

--- 8.cpp
+++ 8.cpp
@@ -15,32 +15,32 @@ int main() {
         commands[i].points = 0;
     }
 
-    commands[1].title = "Краснодар";
+    commands[1].title = "РљСЂР°СЃРЅРѕРґР°СЂ";
     commands[1].city = "Krasnodar";
     commands[1].number_of_players = 42;
     commands[1].points = 8;
 
-    commands[2].title = "Спартак";
+    commands[2].title = "РЎРїР°СЂС‚Р°Рє";
     commands[2].city = "Moscow";
     commands[2].number_of_players = 65;
     commands[2].points = 17;
 
-    commands[3].title = "Амкар";
+    commands[3].title = "РђРјРєР°СЂ";
     commands[3].city = "Perm";
     commands[3].number_of_players = 34;
     commands[3].points = 14;
 
-    commands[4].title = " Зенит";
+    commands[4].title = " Р—РµРЅРёС‚";
     commands[4].city = "Saint-Petersburg";
     commands[4].number_of_players = 89;
     commands[4].points = 35;
 
-    commands[5].title = "Ростов";
+    commands[5].title = "Р РѕСЃС‚РѕРІ";
     commands[5].city = "Rostov_on_Don";
     commands[5].number_of_players = 72;
     commands[5].points = 34;
 
-    commands[6].title = "Динамо";
+    commands[6].title = "Р”РёРЅР°РјРѕ";
     commands[6].city = "Moscow";
     commands[6].number_of_players = 63;
     commands[6].points = 16;
@@ -48,15 +48,15 @@ int main() {
     for (int i = 1; i <= 10; i++) {
         if (commands[i].points > 0) {
             cout << i << ")";
-            cout << "Название: " << commands[i].title << endl;
-            cout << "Город: " << commands[i].city << endl;
-            cout << "Игроки: " << commands[i].number_of_players << endl;
-            cout << "Очки: " << commands[i].points << endl << endl;
+            cout << "РќР°Р·РІР°РЅРёРµ: " << commands[i].title << endl;
+            cout << "Р“РѕСЂРѕРґ: " << commands[i].city << endl;
+            cout << "РРіСЂРѕРєРё: " << commands[i].number_of_players << endl;
+            cout << "РћС‡РєРё: " << commands[i].points << endl << endl;
 
         }
     }
     int ptlim = 0;
-    cout << "Введите минимальный количество очков. " << endl;
+    cout << "Р’РІРµРґРёС‚Рµ РЅСѓР¶РЅРѕРµ РєРѕР»РёС‡РµСЃС‚РІРѕ РѕС‡РєРѕРІ " << endl;
     cin >> ptlim;
     for (int i = 1; i <= 10; i++) {
         if (commands[i].points < ptlim) {
@@ -77,42 +77,42 @@ int main() {
             commands[G].number_of_players = 0;
         }
     }
-    cout << "Список команд удовлетворяющих по количеству очков.: " << endl;
+    cout << "РЎРїРёСЃРѕРє РєРѕРјР°РЅРґ СѓРґРѕРІР»РµС‚РІРѕСЂСЏСЋС‰РёС… РїРѕ РєРѕР»РёС‡РµСЃС‚РІСѓ РѕС‡РєРѕРІ.: " << endl;
     for (int i = 1; i <= 10; i++) {
         if (commands[i].points > 0) {
             cout << i << ")";
-            cout << "Название: " << commands[i].title << endl;
-            cout << "Город: " << commands[i].city << endl;
-            cout << "Игроки: " << commands[i].number_of_players << endl;
-            cout << "Очки: " << commands[i].points << endl << endl;
+            cout << "РќР°Р·РІР°РЅРёРµ: " << commands[i].title << endl;
+            cout << "Р“РѕСЂРѕРґ: " << commands[i].city << endl;
+            cout << "РРіСЂРѕРєРё: " << commands[i].number_of_players << endl;
+            cout << "РћС‡РєРё: " << commands[i].points << endl << endl;
             G = 0;
 
         }
     }
-    cout << "Введите данные об одной команде: " << endl;
+    cout << "Р’РІРµРґРёС‚Рµ РґР°РЅРЅС‹Рµ РѕР± РѕРґРЅРѕР№ РєРѕРјР°РЅРґРµ: " << endl;
     for (int i = G; i < G + 2; i++) {
 
-        cout << "Название: ";
+        cout << "РќР°Р·РІР°РЅРёРµ: ";
         cin >> commands[i].title;
 
-        cout << "Город: ";
+        cout << "Р“РѕСЂРѕРґ: ";
         cin >> commands[i].city;
 
-        cout << "Игроки: ";
+        cout << "РРіСЂРѕРєРё: ";
         cin >> commands[i].number_of_players;
 
-        cout << "Очки: ";
+        cout << "РћС‡РєРё: ";
         cin >> commands[i].points;
         cout << endl;
     }
-    cout << "Измененный список команд: " << endl;
+    cout << "РР·РјРµРЅРµРЅРЅС‹Р№ СЃРїРёСЃРѕРє РєРѕРјР°РЅРґ: " << endl;
     for (int i = 1; i <= 10; i++) {
         if ((commands[i].points > 0) && (commands[i].number_of_players > 0)) {
             cout << i << ")";
-            cout << "Название: " << commands[i].title << endl;
-            cout << "Город: " << commands[i].city << endl;
-            cout << "Игроки: " << commands[i].number_of_players << endl;
-            cout << "Очки: " << commands[i].points << endl << endl;
+            cout << "РќР°Р·РІР°РЅРёРµ: " << commands[i].title << endl;
+            cout << "Р“РѕСЂРѕРґ: " << commands[i].city << endl;
+            cout << "РРіСЂРѕРєРё: " << commands[i].number_of_players << endl;
+            cout << : "РћС‡РєРё << commands[i].points << endl << endl;
             G++;
         }
     }

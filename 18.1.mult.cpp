89c1188f2c49ee53102ca04e003d698203df8b7a
Vladkorekov21 <74411502+Vladkorekov21@users.noreply.github.com>
2021-05-17 14:52:15 +0500

    Create 18.1.mult.cpp

---
+++ 18.1.mult.cpp
@@ -0,0 +1,24 @@
+  
+#include <iostream>
+#include "mult.h"
+
+using namespace std;
+
+void mult::init(int f, double s) {
+	first = f;
+	second = s;
+}
+
+void mult::read() {
+	cout << "first? "; cin >> first;
+	cout << "second? "; cin >> second;
+}
+
+void mult::show() {
+	cout << "first = " << first << endl;
+	cout << "second = " << second << endl;
+}
+
+double mult::multiply(double k) {
+	return (first * k + second * k);
+}

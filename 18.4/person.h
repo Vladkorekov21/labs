aa569ab224225b603aa755463c65faf2c5fee358
Vladkorekov21 <74411502+Vladkorekov21@users.noreply.github.com>
2021-05-24 14:36:54 +0500

    Create person.h

---
+++ 18.4/person.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string> 
+#include <iostream> 
+using namespace std; 
+class Person
+{
+public:
+Person(); 
+	virtual ~Person();
+	Person(string,int);
+	Person( const Person&);
+	string Get_name() {
+		return name; 
+	} 
+	int Get_age() { 
+		return age; 
+	}
+	void Set_name(string); 
+	void Set_age(int);
+	Person& operator=(const Person&);
+	friend istream& operator>>(istream& in, Person& c);
+	friend ostream& operator<<(ostream& out, const Person& c);
+protected:
+	string name;
+	int age;
+};

f150af38a3c5e9d7fdb7879db0b5f18558967940
Vladkorekov21 <74411502+Vladkorekov21@users.noreply.github.com>
2021-05-24 14:27:46 +0500

    Create 18.3.cpp

---
+++ 18.3/18.3.cpp
@@ -0,0 +1,19 @@
+#include "Pair.h" 
+#include <iostream> 
+using namespace std;
+void main()
+{
+	Pair a;
+	Pair b;
+	Pair c;  
+	cin>>a;
+	cin>>b;
+	--a;
+	cout<<a<<endl;
+	c = a--;
+	cout<<"a="<<a<<endl;
+	cout<<"b="<<b<<endl; 
+	cout<<"c="<<c<<endl;
+	cout << "a > b= " << (a > b) << endl;
+	cout << "a < b= " << (a < b) << endl;
+}

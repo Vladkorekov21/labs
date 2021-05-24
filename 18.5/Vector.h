d1a55eb318e65bf3fd009bd4ec348401a99116a6
Vladkorekov21 <74411502+Vladkorekov21@users.noreply.github.com>
2021-05-24 14:49:49 +0500

    Create Vector.h

---
+++ 18.5/Vector.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "object.h" 
+#include <string> 
+#include <iostream>
+
+using namespace std; 
+class Vector
+{
+public:
+	Vector(void);//êîíñòðóêòîð áåç ïàðàìåòðîâ 
+	Vector(int);//êîíñòðóêòîð êîïèðîâàíèÿ
+	~Vector(void);//äåñòðóêòîð
+	void Add(Object*);//äîáàâëåíèå ýëåìåíòà â âåêòîð
+	friend ostream& operator<<(ostream& out, const Vector&);//îïåðàöèÿ âûâîäà 
+protected:
+	Object** beg;//óêàçàòåëü íà ïåðâûé ýëåìåíò âåêòîðà 
+	int size;//ðàçìåð
+	int cur;//òåêóùàÿ ïîçèöèÿ
+};

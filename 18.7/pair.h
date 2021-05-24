509518a6203c13533f951ea451d615bcb0fa64db
Vladkorekov21 <74411502+Vladkorekov21@users.noreply.github.com>
2021-05-24 15:18:02 +0500

    Create pair.h

---
+++ 18.7/pair.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream> 
+using namespace std; 
+class Pair
+{
+public:
+	Pair();
+	Pair(int, double);
+	Pair(const Pair&);
+	Pair& operator=(const Pair&);
+	Pair operator+(const Pair k);
+	friend Pair& operator+=(Pair& x, Pair& y);
+friend ostream& operator<< (ostream& out, const Pair&);
+friend istream& operator>> (istream& in, Pair&);
+public:
+virtual ~Pair() {};
+private:
+	int first;
+	double second;
+};
